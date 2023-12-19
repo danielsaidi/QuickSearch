61ee7d4abac6f38356f2937f926ee94f846ee937
Daniel Saidi <daniel.saidi@gmail.com>
2023-12-19 16:48:20 +0100

    Remove bolierplate test code

--- Tests/QuickSearchTests/QuickSearchTests.swift
+++ Tests/QuickSearchTests/QuickSearchTests.swift
@@ -2,11 +2,6 @@ import XCTest
 @testable import QuickSearch
 
 final class QuickSearchTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
-
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
-    }
+    
+    func testExample() throws {}
 }
